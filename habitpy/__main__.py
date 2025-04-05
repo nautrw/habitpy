41c850ba0e6d4efa49d62c1f5a83ce7b8656d513
nautrw <nautrw@proton.me>
2025-04-05 15:26:07 -0400

    test stuff

--- habitpy/__main__.py
+++ habitpy/__main__.py
@@ -8,6 +8,6 @@ CONNECTION = sqlite3.connect("data.db")
 if __name__ == "__main__":
     backend.create_table(CONNECTION)
 
-    backend.create_habit(CONNECTION, "test", 0, time.time(), time.time())
-    backend.create_habit(CONNECTION, "test2", 0, time.time(), time.time())
-    backend.delete_habit(CONNECTION, 1)
+    backend.create_habit(CONNECTION, "test")
+    backend.create_habit(CONNECTION, "test2")
+    backend.rename_habit(CONNECTION, 2, "test2-renamed")
