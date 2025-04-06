07b068708806dc56aab922f218b157290432b6af
nautrw <nautrw@proton.me>
2025-04-06 13:33:20 -0400

    add help messages

--- habitpy/__main__.py
+++ habitpy/__main__.py
@@ -5,17 +5,23 @@ import typer
 import habitpy.backend as backend
 
 CONNECTION = sqlite3.connect("data.db")
-app = typer.Typer(no_args_is_help=True)
+app = typer.Typer(no_args_is_help=True, help="CLI habit manager")
 
 
 @app.command()
 def create(name: str):
+    """
+    Create a habit
+    """
     id = backend.create_habit(CONNECTION, name)
     print(f"Successfuly created habit '{name}' with ID {id}")
 
 
 @app.command()
 def delete(id: int):
+    """
+    Delete a habit
+    """
     backend.delete_habit(CONNECTION, id)
     print(f"Successfuly deleted habit with ID {id}")
 
