3d69c8771580166e1e85939bdb632f06ac4bc513
nautrw <nautrw@proton.me>
2025-04-06 13:28:24 -0400

    make create habit command in cli

--- habitpy/__main__.py
+++ habitpy/__main__.py
@@ -1,14 +1,20 @@
 import sqlite3
-import time
+
+import typer
 
 import habitpy.backend as backend
 
 CONNECTION = sqlite3.connect("data.db")
+app = typer.Typer(no_args_is_help=True)
+
+
+@app.command()
+def create(name: str):
+    id = backend.create_habit(CONNECTION, name)
+    print(f"Successfuly created habit '{name}' with ID {id}")
+
 
 if __name__ == "__main__":
     backend.create_table(CONNECTION)
 
-    backend.create_habit(CONNECTION, "test")
-    backend.create_habit(CONNECTION, "test2")
-    backend.change_habit_times(CONNECTION, 2, 3)
-    backend.change_habit_times(CONNECTION, 2, -1)
+    app()
